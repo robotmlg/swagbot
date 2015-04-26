2a69ac660d32c2df5f6c167df40aa8eb64202988
Greekguy <tnatsis@yahoo.com>
2015-04-26 16:50:01 -0400

    Added ability to find date of birth to wikistuff

--- Thanassi/wikistuff.py
+++ Thanassi/wikistuff.py
@@ -22,4 +22,22 @@ def findTheDeath(url):
                 elif s[j] == '>' and s[j+1] != '<' and s[j+3] != '<':
                     record = True
 
-print findTheDeath(t6)
+#print findTheDeath(t6)
+
+def findTheBirth(url):
+    f = urlopen(url)
+    s = f.read()
+    f.close()
+    for i in range(len(s)):
+        if s[i] == 'D' and s[i+1] == 'a' and s[i+2] == 't' and s[i+3] == 'e' and s[i+4] == ' ' and s[i+5] == 'o' and s[i+6] == 'f' and s[i+7] == ' 'and s[i+8] == 'b':
+            record = False
+            result = ""
+            for j in range(i+18, len(s)):
+                if record == True and s[j] == '<':
+                    return result
+                elif record == True:
+                    result += s[j]
+                elif s[j] == '>' and s[j+1] != '<' and s[j+3] != '<':
+                    record = True
+
+#print findTheBirth(t1)
